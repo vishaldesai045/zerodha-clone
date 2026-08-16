8538307e8c60f2d94112ad7ae999c6af5fb5683c
vishal desai <desaivishal6090@outlook.com>
2026-08-16 14:25:28 +0530

    Update API URLs to Render backend

--- dashboard/src/components/Holdings.js
+++ dashboard/src/components/Holdings.js
@@ -8,7 +8,7 @@ const Holdings = () => {
   const [allHoldings, setAllHoldings] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:3002/allHoldings").then((res) => {
+    axios.get("https://zerodha-clone-ljmd.onrender.com/allHoldings").then((res) => {
       // console.log(res.data);
       setAllHoldings(res.data);
     });
