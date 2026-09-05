9aa5fabca5a934ff6708647acd3c86a931ec0aee
vishal desai <desaivishal6090@outlook.com>
2026-09-05 22:44:01 +0530

    Fix Render API root route

--- backend/index.js
+++ backend/index.js
@@ -18,6 +18,10 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+app.get("/", (req, res) => {
+  res.send("Zerodha clone API is running");
+});
+
 // app.get("/addHoldings", async (req, res) => {
 //   let tempHoldings = [
 //     {
